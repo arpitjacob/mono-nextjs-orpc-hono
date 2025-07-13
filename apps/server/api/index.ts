41cd881267a3df32fa93ae9801c415f4144dbbed
Arpit Jacob <arpit.jacob@gmail.com>
2025-07-13 14:29:35 +0200

    Initial commit
    Update .env.example
    Run lint
    Move from src to api dir for Vercel
    Fix paths

---
+++ apps/server/api/index.ts
@@ -0,0 +1,63 @@
+import "dotenv/config";
+import { RPCHandler } from "@orpc/server/fetch";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { handle } from "hono/vercel";
+import { auth } from "./lib/auth";
+import { createContext } from "./lib/context";
+import { appRouter } from "./routers/index";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(
+  "/*",
+  cors({
+    origin: process.env.CORS_ORIGIN || "",
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
+
+const orpcHandler = new RPCHandler(appRouter);
+
+app.use("/rpc/*", async (c, next) => {
+  const context = await createContext({ context: c });
+  const { matched, response } = await orpcHandler.handle(c.req.raw, {
+    prefix: "/rpc",
+    context: context,
+  });
+
+  if (matched) {
+    return c.newResponse(response.body, response);
+  }
+  await next();
+});
+
+app.get("/", (c) => {
+  return c.text("OK");
+});
+
+// import { serve } from "@hono/node-server";
+
+// serve(
+//   {
+//     fetch: app.fetch,
+//     port: 3000,
+//   },
+//   (info) => {
+//     console.log(`Server is running on http://localhost:${info.port}`);
+//   },
+// );
+
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
